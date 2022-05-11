5e671b2eb603e00483a87a155ac22a1de5d695f5
CalsRanna <calsranna@gmail.com>
2022-05-12 02:48:35 +0800

    feat(docs): update online docs

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "e53008fb2df774aa086269767a357e55",
+  "assets/AssetManifest.json": "a2fd9065ebbc50c2e9ee9baf49b7df1b",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/fonts/MaterialIcons-Regular.otf": "7e7a6cccddf6d7b20012a548461d5d81",
 "assets/NOTICES": "85f11bd194dccc54e63f75e3142ea8ef",
@@ -13,7 +13,7 @@ const RESOURCES = {
 "assets/snippet/button.type.snippet": "30ecfd734a496f7ac875a5f2bf510f0c",
 "assets/snippet/card.classic.snippet": "54831e7ec28acf884c7887dae6f9133f",
 "assets/snippet/checkbox.basic.snippet": "1f2c3b0a51b833efc6a63700ac77ef20",
-"assets/snippet/demo.dart": "be28373a8fbb7fa979deee0199765d6c",
+"assets/snippet/demo.dart": "f1960eeedb91b175575b0237f19d17bf",
 "assets/snippet/divider.horizontal.snippet": "990993963d521808fe5fa3179a3e9aa5",
 "assets/snippet/drawer.basic.snippet": "9ba44309bb111713570b029e06e6e7d5",
 "assets/snippet/image.basic.snippet": "4607128a9bef76962ce76335c04871f6",
@@ -21,6 +21,8 @@ const RESOURCES = {
 "assets/snippet/input_number.basic.snippet": "f492c2356911d85ed6fd4832116957af",
 "assets/snippet/menu.inline_collapsed.snippet": "2c84305e8c7b22a7c82703b1d12cd20f",
 "assets/snippet/message.basic.snippet": "be28373a8fbb7fa979deee0199765d6c",
+"assets/snippet/modal.basic.snippet": "2536b46d91ab896150cf457e39e1b5d7",
+"assets/snippet/notification.basic.snippet": "f1960eeedb91b175575b0237f19d17bf",
 "assets/snippet/page_header.standard.snippet": "2256ba0a6617c227231b8a2ee13e53e5",
 "assets/snippet/pagination.basic.snippet": "699a599c084dde45c911aadf21de7c3a",
 "assets/snippet/radio.basic.snippet": "4237a92ee2199dc2ce8c4fe4bd130a5e",
@@ -28,7 +30,7 @@ const RESOURCES = {
 "assets/snippet/slider.basic.snippet": "aeef66b6483f554952f0fc0ecba2c9e0",
 "assets/snippet/space.basic.snippet": "1e50d3029bd60572edf21fb3a3a1aa89",
 "assets/snippet/switch.basic.snippet": "48894a3b454fde9f9840f11cb9f4e5c5",
-"assets/snippet/table.basic.snippet": "f0cb1c604cf187c97f3aadbae2fdcbe4",
+"assets/snippet/table.basic.snippet": "3d39743cd2f743522e847d239833a146",
 "assets/snippet/tabs.basic.snippet": "69d1208aa1cf019dca087e060963f934",
 "assets/snippet/tag.basic.snippet": "235a33d47cfbe61c860289a690c2314e",
 "assets/snippet/tooltip.basic.snippet": "7fab75377f8514208de22ab3fe9e31e6",
@@ -42,9 +44,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "ade5be59a1e1d2fb979e0a9510ac0cf8",
-"/": "ade5be59a1e1d2fb979e0a9510ac0cf8",
-"main.dart.js": "2bcaf0349b5f252820ef577f3d11d688",
+"index.html": "20254fcc444cdafd9a7efd0ce650fd25",
+"/": "20254fcc444cdafd9a7efd0ce650fd25",
+"main.dart.js": "ed70d9095b54cf10370f94759cb257a5",
 "manifest.json": "5a9eb9da0a1d5d86f6bca320632a4093",
 "version.json": "350ad229dcbadce74e62729cdfbeb20e"
 };
